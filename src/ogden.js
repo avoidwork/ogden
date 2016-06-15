4cb6463670d493cbefe51a385d1c3f1a52c61b88
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-06-15 00:38:05 -0400

    Debugging in progress

--- src/ogden.js
+++ src/ogden.js
@@ -3,7 +3,7 @@ function ogden (input = "") {
 
 	if (input.length > 0) {
 		result = {
-			operations: input.match(new RegExp(operations.join("|")), "ig") || [],
+			operations: input.match(new RegExp("\\s*?(" + operations.join("|") + ")\\s*?", "ig")) || [],
 			thingsGeneral: input.match(new RegExp(thingsGeneral.join("|")), "ig") || [],
 			thingsPicturable: input.match(new RegExp(thingsPicturable.join("|")), "ig") || [],
 			qualitiesGeneral: input.match(new RegExp(qualitiesGeneral.join("|")), "ig") || [],
