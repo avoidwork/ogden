d138000e17f24be5cf995a5130a5600e8bc0057c
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-10-25 08:45:51 -0400

    Fixing `filter()` to drop duplicates

--- src/filter.js
+++ src/filter.js
@@ -1,1 +1,7 @@
-	const filter = (input = [], words = new Set()) => input.filter(i => words.has(i)).sort();
+	const filter = (input = [], words = new Set()) => input.filter(i => words.has(i)).sort().reduce((a, b) => {
+		if (a.includes(b) === false) {
+			a.push(b);
+		}
+
+		return a;
+	}, []);
